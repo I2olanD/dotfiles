7e57864fcae25e7ad3db463d1bfcf654a9cb0758
Roland <roland.olah@soprabanking.com>
2024-03-15 15:54:05 +0100

    feat: Update lsp

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -4,26 +4,47 @@ return {
     dependencies = {
       {
         "L3MON4D3/LuaSnip",
-        build = (function()
-          if vim.fn.has("win32") == 1 then
-            return
-          end
-          return "make install_jsregexp"
-        end)(),
+        dependencies = { "rafamadriz/friendly-snippets" },
       },
       "saadparwaiz1/cmp_luasnip",
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-path",
       "rafamadriz/friendly-snippets",
+
+      "onsails/lspkind.nvim"
     },
 
     config = function()
       local cmp = require("cmp")
       local luasnip = require("luasnip")
+      local lspkind = require('lspkind')
 
       require("luasnip.loaders.from_vscode").lazy_load()
+
       luasnip.config.setup({})
+
       cmp.setup({
+        window = {
+          completion = {
+            border = "rounded",
+            winhighlight = "Normal:CmpNormal"
+          },
+          documentation = {
+            border = "rounded",
+            winhighlight = "Normal:CmpDocNormal"
+          }
+        },
+        formatting = {
+          format = lspkind.cmp_format({
+          mode = "symbol_text",
+          maxwidth = 50,
+          ellipsis_char = '...',
+          show_labelDetails = true,
+          before = function (entry, vim_item)
+            return vim_item
+          end
+          })
+        },
         snippet = {
           expand = function(args)
             luasnip.lsp_expand(args.body)
@@ -67,7 +88,7 @@ return {
         sources = {
           { name = "nvim_lsp" },
           { name = "luasnip" },
-          { name = "path" },
+          { name = "buffer" },
         },
       })
     end,
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,114 +1,31 @@
+local servers = {
+  omnisharp = {},
+  tsserver = { filetypes = { "typescript", "typescriptreact", "typescript.tsx" } },
+}
+
 return {
-  {
-    "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-    init = function()
-      vim.diagnostic.config({
-        severity_sort = true,
-
-        virtual_lines = false,
-        virtual_text = true,
-      })
-    end,
-    keys = {
-      {
-        "<leader>d",
-        function()
-          local virtual_lines = vim.diagnostic.config().virtual_lines
-          local virtual_text = vim.diagnostic.config().virtual_text
-
-          vim.diagnostic.config({
-            virtual_lines = not virtual_lines,
-            virtual_text = not virtual_text,
-          })
-        end,
-        desc = "Toggle LSP [L]ines",
-      },
-    },
-    config = function()
-      require("lsp_lines").setup()
-    end,
-  },
   {
     "neovim/nvim-lspconfig",
     dependencies = {
       "williamboman/mason.nvim",
       "williamboman/mason-lspconfig.nvim",
-
-      "neovim/nvim-lspconfig",
-      "nvim-lua/plenary.nvim",
       "hrsh7th/cmp-nvim-lsp",
-
-      "jay-babu/mason-null-ls.nvim",
-      "nvimtools/none-ls.nvim",
     },
-
     config = function()
       require("mason").setup()
-
-      local on_attach = function(_, bufnr)
-        local nmap = function(keys, func, desc)
-          if desc then
-            desc = "LSP: " .. desc
-          end
-
-          vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
-        end
-
-        nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-        nmap("<leader>gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-        nmap("<leader>gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-
-        vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
-          vim.lsp.buf.format()
-        end, { desc = "Format current buffer with LSP" })
-      end
-
-      -- see@https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-      local servers = {
-        sqls = {},
-        gopls = {},
-        omnisharp = {},
-        eslint = {
-          packageManager = "yarn",
-          workingDirectories = {
-            { pattern = "./packages/*" },
-          },
-        },
-        tsserver = { filetypes = { "typescript", "typescriptreact", "typescript.tsx" } },
-        html = { filetypes = { "html", "twig", "hbs" } },
-        lua_ls = {
-          Lua = {
-            workspace = { checkThirdParty = false },
-            telemetry = { enable = false },
-            diagnostics = { globals = { "vim" } },
-          },
-        },
-        vimls = {},
-      }
-
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
-
       require("mason-lspconfig").setup({
         ensure_installed = vim.tbl_keys(servers),
-
         handlers = {
           function(server_name)
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
+            capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+
             require("lspconfig")[server_name].setup({
-              capabilities = capabilities,
-              on_attach = on_attach,
-              settings = servers[server_name],
-              filetypes = (servers[server_name] or {}).filetypes,
+              capabilities = capabilities
             })
-          end,
-        },
+          end
+        }
       })
-
-      require("mason-null-ls").setup({
-        ensure_installed = { "sqlfmt", "stylua", "prettier", "goimports", "gofumpt", "yamlfmt" },
-      })
-
-      require("null-ls").setup({})
-    end,
-  },
+    end
+  }
 }
