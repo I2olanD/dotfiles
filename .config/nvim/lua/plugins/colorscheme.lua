9526ae3010943eef0eb939ea91d0139b43131167
Roland Olah <rolandolah@mbp-von-admin.speedport.ip>
2024-05-22 13:50:25 +0200

    feat: Update themes

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,4 +1,13 @@
 return {
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   opts = {},
+  --   config = function()
+  --     vim.cmd.colorscheme("tokyonight")
+  --   end
+  -- }
   {
     "catppuccin/nvim",
     name = "catppuccin",
