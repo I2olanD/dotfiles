a8afa874e465c9fcba68c628981cbefc262d7125
Roland Olah <rolandolah@icloud.com>
2024-12-24 10:40:12 +0100

    feat: Update configs

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,31 +1,33 @@
 local servers = {
   omnisharp = {},
-  ts_ls = { filetypes = { "typescript", "typescriptreact", "typescript.tsx" } },
+  ts_ls = { filetypes = { "javascriptreact", "typescript", "typescriptreact", "typescript.tsx" } },
   gopls = {},
   lua_ls = {
     Lua = {
       workspace = { checkThirdParty = false },
       telemetry = { enable = false },
-      diagnostics = { globals = { "vim" } }
-    }
-  }
+      diagnostics = { globals = { "vim" } },
+    },
+  },
 }
 
 local on_attach = function(_, bufnr)
-  vim.keymap.set("n", "<leader>gD", function() vim.lsp.buf.declaration() end,
-    { buffer = bufnr, desc = "[G]o to [D]eclaration" })
-  vim.keymap.set("n", "<leader>gd", function() vim.lsp.buf.definition() end,
-    { buffer = bufnr, desc = "[G]o to [D]efinition" })
-  vim.keymap.set("n", "<leader>gi", function() vim.lsp.buf.implementation() end,
-    { buffer = bufnr, desc = "[G]o to [I]mplementation" })
-  vim.keymap.set("n", "<leader>k", function() vim.lsp.buf.code_action() end,
-    { buffer = bufnr, desc = "[C]ode action" })
+  vim.keymap.set("n", "<leader>gD", function()
+    vim.lsp.buf.declaration()
+  end, { buffer = bufnr, desc = "[G]o to [D]eclaration" })
+  vim.keymap.set("n", "<leader>gd", function()
+    vim.lsp.buf.definition()
+  end, { buffer = bufnr, desc = "[G]o to [D]efinition" })
+  vim.keymap.set("n", "<leader>gi", function()
+    vim.lsp.buf.implementation()
+  end, { buffer = bufnr, desc = "[G]o to [I]mplementation" })
+  vim.keymap.set("n", "<leader>k", function()
+    vim.lsp.buf.code_action()
+  end, { buffer = bufnr, desc = "[C]ode action" })
 
-  vim.api.nvim_buf_create_user_command(
-    bufnr,
-    "Format",
-    function(_) vim.lsp.buf.format() end,
-    { desc = "Format current buffer with LSP" })
+  vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
+    vim.lsp.buf.format()
+  end, { desc = "Format current buffer with LSP" })
 end
 
 local handlers = {
@@ -50,23 +52,22 @@ return {
             local capabilities = vim.lsp.protocol.make_client_capabilities()
             capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
-
             require("lspconfig")[server_name].setup({
               capabilities = capabilities,
               settings = servers[server_name],
               filetypes = (servers[server_name] or {}).filetypes,
               on_attach = on_attach,
-              handlers = handlers
+              handlers = handlers,
             })
 
             require("lspconfig").dartls.setup({
               capabilities = capabilities,
               on_attach = on_attach,
-              handlers = handlers
+              handlers = handlers,
             })
-          end
-        }
+          end,
+        },
       })
-    end
-  }
+    end,
+  },
 }
