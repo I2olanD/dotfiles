c39a162711a6379f49510b636777797bfba16de6
Roland Wallner <rolandolah@icloud.com>
2025-05-20 16:55:24 +0200

    feat: Update config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -2,36 +2,59 @@ local servers = {
   ts_ls = { filetypes = { "javascriptreact", "typescript", "typescriptreact", "typescript.tsx" } },
   gopls = {},
   lua_ls = {
-    Lua = {
-      workspace = { checkThirdParty = false },
+    lua = {
+      workspace = { checkthirdparty = false },
       telemetry = { enable = false },
       diagnostics = { globals = { "vim" } },
     },
   },
 }
 
+-- Create a special setup function for dart_ls that uses FVM
+local setup_dart_ls = function(server_name, config)
+  local util = require("lspconfig.util")
+  local project_root = util.find_git_ancestor(vim.fn.getcwd()) or vim.fn.getcwd()
+  local fvm_dart_path = project_root .. "/.fvm/flutter_sdk/bin/dart"
+
+  -- Check if FVM Dart exists
+  if vim.fn.filereadable(fvm_dart_path) == 1 then
+    config.cmd = {
+      fvm_dart_path,
+      "language-server",
+      "--protocol=lsp"
+    }
+  else
+    vim.notify("FVM Dart SDK not found at: " .. fvm_dart_path, vim.log.levels.WARN)
+    -- Fallback to system Dart if available
+    if vim.fn.executable("dart") == 1 then
+      config.cmd = { "dart", "language-server", "--protocol=lsp" }
+    end
+  end
+
+  require('lspconfig')[server_name].setup(config)
+end
+
 local on_attach = function(client, bufnr)
-  vim.keymap.set("n", "<leader>gD", function()
+  vim.keymap.set("n", "<leader>gd", function()
     vim.lsp.buf.declaration()
-  end, { buffer = bufnr, desc = "[G]o to [D]eclaration" })
+  end, { buffer = bufnr, desc = "[g]o to [d]eclaration" })
   vim.keymap.set("n", "<leader>gd", function()
     vim.lsp.buf.definition()
-  end, { buffer = bufnr, desc = "[G]o to [D]efinition" })
+  end, { buffer = bufnr, desc = "[g]o to [d]efinition" })
   vim.keymap.set("n", "<leader>gi", function()
     vim.lsp.buf.implementation()
-  end, { buffer = bufnr, desc = "[G]o to [I]mplementation" })
+  end, { buffer = bufnr, desc = "[g]o to [i]mplementation" })
   vim.keymap.set("n", "<leader>k", function()
     vim.lsp.buf.code_action()
-  end, { buffer = bufnr, desc = "[C]ode action" })
-
-  vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
+  end, { buffer = bufnr, desc = "[c]ode action" })
+  vim.api.nvim_buf_create_user_command(bufnr, "format", function(_)
     vim.lsp.buf.format()
-  end, { desc = "Format current buffer with LSP" })
+  end, { desc = "format current buffer with lsp" })
 end
 
 local handlers = {
-  ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" }),
-  ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" }),
+  ["textdocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" }),
+  ["textdocument/signaturehelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" }),
 }
 
 return {
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -64,5 +64,6 @@ return {
     telescope.load_extension("fzf")
     telescope.load_extension("file_browser")
     telescope.load_extension("ui-select")
+    telescope.load_extension("flutter")
   end,
 }
