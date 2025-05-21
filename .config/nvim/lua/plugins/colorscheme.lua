ba41c6c25c696a8ac74318a9db4040116ed2d61a
Roland Wallner <rolandolah@icloud.com>
2025-05-21 13:34:28 +0200

    feat: Update theme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,29 +1,29 @@
 return {
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-    config = function()
-      require("tokyonight").setup()
-
-      vim.cmd.colorscheme("tokyonight-night")
-    end,
-  }
   -- {
-  --   "catppuccin/nvim",
-  --   name = "catppuccin",
+  --   "folke/tokyonight.nvim",
+  --   lazy = false,
   --   priority = 1000,
+  --   opts = {},
   --   config = function()
-  --     require("catppuccin").setup({
-  --       flavour = "mocha",
-  --       transparent_background = true,
-  --       styles = {
-  --         comments = { "italic" },
-  --       },
-  --     })
+  --     require("tokyonight").setup()
   --
-  --     vim.cmd.colorscheme("catppuccin")
+  --     vim.cmd.colorscheme("tokyonight-night")
   --   end,
-  -- },
+  -- }
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha",
+        transparent_background = true,
+        styles = {
+          comments = { "italic" },
+        },
+      })
+
+      vim.cmd.colorscheme("catppuccin")
+    end
+  }
 }
