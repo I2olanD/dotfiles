7108cdb2a5b463abf6a4efe09a614d74d4a17465
Roland Wallner <rolandolah@icloud.com>
2025-12-29 08:55:53 +0100

    feat: Introduce biome

--- .config/nvim/lua/plugins/lint.lua
+++ .config/nvim/lua/plugins/lint.lua
@@ -2,15 +2,25 @@ return {
   "mfussenegger/nvim-lint",
   event = { "BufWritePost", "BufReadPost", "InsertLeave" },
   config = function()
-    require("lint").linters_by_ft = {
+    local lint = require("lint")
+
+    local function has_biome_config()
+      local root = vim.fn.getcwd()
+      return vim.fn.filereadable(root .. "/biome.json") == 1
+        or vim.fn.filereadable(root .. "/biome.jsonc") == 1
+    end
+
+    local js_linters = has_biome_config() and { "biomejs" } or { "eslint" }
+
+    lint.linters_by_ft = {
       lua = { "luacheck" },
-      json = { "jsonlint" },
-      javascript = { "eslint" },
-      typescript = { "eslint" },
-      javascriptreact = { "eslint" },
-      typescriptreact = { "eslint" },
+      json = has_biome_config() and { "biomejs" } or { "jsonlint" },
+      javascript = js_linters,
+      typescript = js_linters,
+      javascriptreact = js_linters,
+      typescriptreact = js_linters,
       sql = { "sqlfluff" },
-      svelte = { "eslint" },
+      svelte = js_linters,
     }
   end,
 }
