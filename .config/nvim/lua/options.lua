8bd169491f3d4b3369914255de6875b99e545fd3
Roland Wallner <rolandolah@icloud.com>
2026-04-21 13:12:17 +0200

    feat: cleanup

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -23,20 +23,13 @@ vim.opt.termguicolors = true
 
 vim.opt.completeopt = { "menuone", "noselect" }
 vim.opt.clipboard = "unnamedplus"
-vim.opt.mouse = "a"
-
 vim.opt.signcolumn = "yes"
 vim.opt.cursorline = true
 vim.opt.timeoutlen = 500
-vim.opt.fileencoding = "utf-8"
 
--- line wrap
-vim.opt.wrap = true
 vim.opt.linebreak = true
 vim.opt.breakindent = true
 
--- search
-vim.opt.hlsearch = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
@@ -48,12 +41,11 @@ vim.opt.backup = false
 vim.opt.swapfile = false
 vim.opt.writebackup = false
 
--- tabs and indentation
-vim.opt.shiftround = true -- Round indent to multiple of `shiftwidth`
-vim.opt.smartindent = true -- do smart indentation when starting a new line
-vim.opt.tabstop = 2 -- number of spaces a <Tab> counts for
-vim.opt.shiftwidth = 2 -- number of psaces to use for each step of indent
-vim.opt.expandtab = true -- always use spaces instead of tabs
+vim.opt.shiftround = true
+vim.opt.smartindent = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
 
 -- cmd window
 vim.o.cmdheight = 0
