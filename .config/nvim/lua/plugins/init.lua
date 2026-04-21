a90452c9cd1dd9c586ff8351dec0051bfa64e73a
Roland Wallner <rolandolah@icloud.com>
2026-04-21 10:34:37 +0200

    feat: Introduce folding

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -18,52 +18,60 @@ vim.api.nvim_create_autocmd("PackChanged", {
 })
 
 vim.pack.add({
+  { src = "https://github.com/saghen/blink.cmp", version = vim.version.range("1.*") },
+
+  "https://github.com/akinsho/bufferline.nvim",
+
   { src = "https://github.com/catppuccin/nvim", name = "catppuccin" },
 
-  "https://github.com/nvim-tree/nvim-web-devicons",
-  "https://github.com/nvim-mini/mini.icons",
+  "https://github.com/stevearc/conform.nvim",
+
+  "https://github.com/lewis6991/gitsigns.nvim",
+
+  "https://github.com/sindrets/diffview.nvim",
+
+  "https://github.com/lukas-reineke/indent-blankline.nvim",
+
+  "https://github.com/mfussenegger/nvim-lint",
 
   "https://github.com/mason-org/mason.nvim",
   "https://github.com/mason-org/mason-lspconfig.nvim",
   "https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim",
   "https://github.com/neovim/nvim-lspconfig",
 
-  "https://github.com/L3MON4D3/LuaSnip",
+  "https://github.com/nvim-lualine/lualine.nvim",
+
+  "https://github.com/stevearc/oil.nvim",
 
   { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
-  { src = "https://github.com/saghen/blink.cmp", version = vim.version.range("1.*") },
+
+  "https://github.com/anuvyklack/windows.nvim",
+
+  "https://github.com/nvim-tree/nvim-web-devicons",
+  "https://github.com/nvim-mini/mini.icons",
+
+  "https://github.com/L3MON4D3/LuaSnip",
+
   "https://github.com/windwp/nvim-ts-autotag",
   "https://github.com/jlcrochet/vim-razor",
-
-  "https://github.com/stevearc/conform.nvim",
-  "https://github.com/mfussenegger/nvim-lint",
-
-  "https://github.com/nvim-lualine/lualine.nvim",
-  "https://github.com/akinsho/bufferline.nvim",
 
   "https://github.com/MunifTanjim/nui.nvim",
   "https://github.com/rcarriga/nvim-notify",
   "https://github.com/catgoose/nvim-colorizer.lua",
-  "https://github.com/lukas-reineke/indent-blankline.nvim",
 
   "https://github.com/ibhagwan/fzf-lua",
-  "https://github.com/stevearc/oil.nvim",
   "https://github.com/folke/trouble.nvim",
 
   "https://github.com/kylechui/nvim-surround",
   "https://github.com/windwp/nvim-autopairs",
   "https://github.com/mg979/vim-visual-multi",
 
-  "https://github.com/anuvyklack/windows.nvim",
   "https://github.com/anuvyklack/middleclass",
   "https://github.com/anuvyklack/animation.nvim",
   "https://github.com/christoomey/vim-tmux-navigator",
   "https://github.com/folke/which-key.nvim",
 
   "https://github.com/enochchau/nvim-pretty-ts-errors",
-
-  "https://github.com/lewis6991/gitsigns.nvim",
-  "https://github.com/sindrets/diffview.nvim",
 
   "https://github.com/SmiteshP/nvim-navic",
   "https://github.com/utilyre/barbecue.nvim",
@@ -92,23 +100,6 @@ vim.schedule(function()
   require("fzf-lua").register_ui_select()
   require("trouble").setup()
   require("colorizer").setup()
-  require("plugins.git")
   require("barbecue").setup()
+  require("plugins.git")
 end)
-
-vim.api.nvim_create_autocmd("LspAttach", {
-  once = true,
-  callback = function()
-    require("lsp-lens").setup()
-    require("goto-preview").setup()
-    require("inc_rename").setup()
-  end,
-})
-
-vim.api.nvim_create_autocmd("InsertEnter", {
-  once = true,
-  callback = function()
-    require("plugins.blink")
-    require("nvim-autopairs").setup()
-  end,
-})
